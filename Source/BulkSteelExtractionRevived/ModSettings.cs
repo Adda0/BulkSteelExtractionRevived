3946c28aaf34e5a1fca6a84708269533ea93f804
Adda0 <chocholaty.david0@gmail.com>
2020-06-22 15:07:58 +0200

    Fix values from sliders in mod settings

--- Source/BulkSteelExtractionRevived/ModSettings.cs
+++ Source/BulkSteelExtractionRevived/ModSettings.cs
@@ -25,9 +25,9 @@ namespace BulkSteelExtractionRevived
             for (var i = 0; (!foundExtractMetalFromSlag || !foundBulkExtractMetalFromSlag || !foundExtractMetalFromDebris || !foundBulkExtractMetalFromDebris) && i < RecipeDefs.Count; i++) {
                 // extract steel from slag
                 if (!foundExtractMetalFromSlag && RecipeDefs[i].defName == "ExtractMetalFromSlag") {
-                    RecipeDefs[i].workAmount = (int)Controller.Settings.workAmountFromSlag;
+                    RecipeDefs[i].workAmount = Controller.Settings.workAmountFromSlag;
                     RecipeDefs[i].products.Clear();
-                    RecipeDefs[i].products.Add(new ThingDefCountClass(thingDef, (int)Controller.Settings.steelAmountFromSlag));
+                    RecipeDefs[i].products.Add(new ThingDefCountClass(thingDef, Controller.Settings.steelAmountFromSlag));
                     if (Controller.Settings.componentFromSlag.Equals(true)) {
                         RecipeDefs[i].products.Add(new ThingDefCountClass(ThingDefOf.ComponentIndustrial, 1));
                     }
@@ -36,7 +36,7 @@ namespace BulkSteelExtractionRevived
                 {
                     RecipeDefs[i].workAmount = (int)(Controller.Settings.workAmountFromSlag * 3 * 0.75);
                     RecipeDefs[i].products.Clear();
-                    RecipeDefs[i].products.Add(new ThingDefCountClass(thingDef, (int)Controller.Settings.steelAmountFromSlag * 3));
+                    RecipeDefs[i].products.Add(new ThingDefCountClass(thingDef, Controller.Settings.steelAmountFromSlag * 3));
                     if (Controller.Settings.componentFromSlag.Equals(true)) {
                         RecipeDefs[i].products.Add(new ThingDefCountClass(ThingDefOf.ComponentIndustrial, 3));
                     }
@@ -46,9 +46,9 @@ namespace BulkSteelExtractionRevived
                 // extract steel from debris
                 else if (!foundExtractMetalFromDebris && RecipeDefs[i].defName == "ExtractMetalFromDebris")
                 {
-                    RecipeDefs[i].workAmount = (int)Controller.Settings.workAmountFromDebris;
+                    RecipeDefs[i].workAmount = Controller.Settings.workAmountFromDebris;
                     RecipeDefs[i].products.Clear();
-                    RecipeDefs[i].products.Add(new ThingDefCountClass(thingDef, (int)Controller.Settings.steelAmountFromDebris));
+                    RecipeDefs[i].products.Add(new ThingDefCountClass(thingDef, Controller.Settings.steelAmountFromDebris));
                     if (Controller.Settings.componentFromDebris.Equals(true)) {
                         RecipeDefs[i].products.Add(new ThingDefCountClass(ThingDefOf.ComponentIndustrial, 1));
                     }
@@ -57,7 +57,7 @@ namespace BulkSteelExtractionRevived
                 {
                     RecipeDefs[i].workAmount = (int)(Controller.Settings.workAmountFromDebris * 3 * 0.75);
                     RecipeDefs[i].products.Clear();
-                    RecipeDefs[i].products.Add(new ThingDefCountClass(thingDef, (int)(Controller.Settings.steelAmountFromDebris * 3)));
+                    RecipeDefs[i].products.Add(new ThingDefCountClass(thingDef, Controller.Settings.steelAmountFromDebris * 3));
                     if (Controller.Settings.componentFromDebris.Equals(true)) {
                         RecipeDefs[i].products.Add(new ThingDefCountClass(ThingDefOf.ComponentIndustrial, 3));
                     }
@@ -77,12 +77,12 @@ namespace BulkSteelExtractionRevived
     }
 
     public class Settings : ModSettings {
-        public float steelAmountFromSlag = 20.0f;
-        public float workAmountFromSlag = 1600.0f;
+        public int steelAmountFromSlag = 20;
+        public int workAmountFromSlag = 1600;
         public bool componentFromSlag = false;
 
-        public float steelAmountFromDebris = 10.0f;
-        public float workAmountFromDebris = 1600.0f;
+        public int steelAmountFromDebris = 10;
+        public int workAmountFromDebris = 1600;
         public bool componentFromDebris = false;
 
         public void DoWindowContents(Rect canvas) {
@@ -91,15 +91,15 @@ namespace BulkSteelExtractionRevived
             list.Begin(canvas);
             list.Gap();
 
-            list.Label("BSER.SteelAmountFromSlag".Translate()+"  "+(int)steelAmountFromSlag);
-            steelAmountFromSlag = list.Slider(steelAmountFromSlag, 1f, 300.99f);
+            list.Label("BSER.SteelAmountFromSlag".Translate() + "  " + steelAmountFromSlag);
+            steelAmountFromSlag = (int) list.Slider(steelAmountFromSlag, 1f, 200.99f);
             Text.Font = GameFont.Tiny;
             list.Label("          "+"BSER.SteelAmountTipFromSlag".Translate());
             Text.Font = GameFont.Small;
             list.Gap();
 
-            list.Label("BSER.WorkAmountFromSlag".Translate()+"  "+(int)((workAmountFromSlag/60)+.5));
-            workAmountFromSlag = list.Slider(workAmountFromSlag, 575f, 2400f);
+            list.Label("BSER.WorkAmountFromSlag".Translate() + "  " + workAmountFromSlag/60);
+            workAmountFromSlag = (int) list.Slider(workAmountFromSlag, 575f, 2400f);
             Text.Font = GameFont.Tiny;
             list.Label("          "+"BSER.WorkAmountTipFromSlag".Translate());
             Text.Font = GameFont.Small;
@@ -108,15 +108,15 @@ namespace BulkSteelExtractionRevived
             list.CheckboxLabeled( "BSER.ComponentFromSlag".Translate(), ref componentFromSlag);
             list.Gap();
 
-            list.Label("BSER.SteelAmountFromDebris".Translate()+"  "+(int)steelAmountFromDebris);
-            steelAmountFromDebris = list.Slider(steelAmountFromDebris, 1f, 300.99f);
+            list.Label("BSER.SteelAmountFromDebris".Translate() + "  " + steelAmountFromDebris);
+            steelAmountFromDebris = (int) list.Slider(steelAmountFromDebris, 1f, 200.99f);
             Text.Font = GameFont.Tiny;
             list.Label("          "+"BSER.SteelAmountTipFromDebris".Translate());
             Text.Font = GameFont.Small;
             list.Gap();
 
-            list.Label("BSER.WorkAmountFromDebris".Translate()+"  "+(int)((workAmountFromDebris/60)+.5));
-            workAmountFromDebris = list.Slider(workAmountFromDebris, 575f, 2400f);
+            list.Label("BSER.WorkAmountFromDebris".Translate() + "  " + workAmountFromDebris/60);
+            workAmountFromDebris = (int) list.Slider(workAmountFromDebris, 575f, 2400f);
             Text.Font = GameFont.Tiny;
             list.Label("          "+"BSER.WorkAmountTipFromDebris".Translate());
             Text.Font = GameFont.Small;
@@ -129,12 +129,12 @@ namespace BulkSteelExtractionRevived
         }
         public override void ExposeData() {
             base.ExposeData();
-            Scribe_Values.Look(ref steelAmountFromSlag, "steelAmountFromSlag", 20.0f);
-            Scribe_Values.Look(ref workAmountFromSlag, "workAmountFromSlag", 1600.0f);
+            Scribe_Values.Look(ref steelAmountFromSlag, "steelAmountFromSlag", 20);
+            Scribe_Values.Look(ref workAmountFromSlag, "workAmountFromSlag", 1600);
             Scribe_Values.Look(ref componentFromSlag, "componentFromSlag", false);
 
-            Scribe_Values.Look(ref steelAmountFromDebris, "steelAmountFromDebris", 10.0f);
-            Scribe_Values.Look(ref workAmountFromDebris, "workAmountFromDebris", 1600.0f);
+            Scribe_Values.Look(ref steelAmountFromDebris, "steelAmountFromDebris", 10);
+            Scribe_Values.Look(ref workAmountFromDebris, "workAmountFromDebris", 1600);
             Scribe_Values.Look(ref componentFromDebris, "componentFromDebris", false);
         }
     }
